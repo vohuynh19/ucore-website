56da398318890b026ee0bea68c9bd17f8714cb16
vhuynh19 <vohuynh01092002@gmail.com>
2023-05-23 11:39:01 +0700

    feat: Update hot course query

--- ui/organisms/Home/HomeHotCourse/index.tsx
+++ ui/organisms/Home/HomeHotCourse/index.tsx
@@ -6,29 +6,11 @@ import { SizeBox } from "ui/atoms";
 import { CourseCard, HottestCourseHeader } from "ui/molecules";
 
 import { Container } from "./styled";
-import { Key } from "react";
 
 const maxDisplayCourse = 4;
 
 const HomeHotCourse = () => {
-  // TODO: Don't know feature, cannot run
-  //const { data } = useCourseQuery(COURSES_QUERY.TOP_COURSES);
-
-  // return (
-  //   <Container>
-  //     <HottestCourseHeader />
-
-  //     <SizeBox height={48} />
-
-  //     <Row gutter={[32, 32]}>
-  //       {(data || []).slice(0, maxDisplayCourse).map((course: { id: Key | null | undefined; }) => (
-  //         <Col key={course.id} xs={24} sm={12} md={8} lg={6}>
-  //           <CourseCard />
-  //         </Col>
-  //       ))}
-  //     </Row>
-  //   </Container>
-  // );
+  const { data } = useCourseQuery(COURSES_QUERY.TOP_COURSES)({});
 
   return (
     <Container>
@@ -37,7 +19,7 @@ const HomeHotCourse = () => {
       <SizeBox height={48} />
 
       <Row gutter={[32, 32]}>
-        {([]).slice(0, maxDisplayCourse).map((course: { id: Key | null | undefined; }) => (
+        {(data || []).slice(0, maxDisplayCourse).map((course) => (
           <Col key={course.id} xs={24} sm={12} md={8} lg={6}>
             <CourseCard />
           </Col>
