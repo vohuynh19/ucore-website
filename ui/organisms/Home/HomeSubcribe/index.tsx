d5c21dc8781d1cfb1a32abefea10d8ed1fbed28f
hiepcbla5 <hiep.cbla5@gmail.com>
2023-05-25 16:13:49 +0700

    Feedback

--- ui/organisms/Home/HomeSubcribe/index.tsx
+++ ui/organisms/Home/HomeSubcribe/index.tsx
@@ -1,21 +1,50 @@
-import { Input } from "antd";
-import { Button } from "ui/atoms";
+import { Input, Typography, Row, Col } from "antd";
+import { Button, SizeBox } from "ui/atoms";
 import { Container, ContentContainer } from "./styled";
+import { useTranslation } from "react-i18next";
 
 const HomeSubscribe = () => {
+  const { t } = useTranslation("home");
+
   return (
     <Container>
       <ContentContainer>
-        <h1>Hãy để lại email để chúng tôi cập nhập thông tin mới nhất.</h1>
+        <h1>{t("feedbackSen1")}</h1>
 
-        <Input
-          placeholder="Your email address here"
-          suffix={
-            <Button type="primary" size="large">
-              Subcribe
-            </Button>
-          }
-        />
+        <Input size="large" placeholder="Your email address here" />
+
+        <SizeBox height={2} />
+        <Input.TextArea size="large" placeholder="Your feedback" />
+
+        <SizeBox height={2} />
+        <Row align="middle">
+          <Col span={8}>
+            <Typography.Text style={{ textAlign: "end" }}>
+              {t("feedbackSen2")}
+            </Typography.Text>
+          </Col>
+          <Col offset={2} span={14}>
+            <Input placeholder="Your name" />
+          </Col>
+        </Row>
+        <SizeBox height={2} />
+        <Row align="middle">
+          <Col span={8}>
+            <Typography.Text>{t("feedbackSen3")}</Typography.Text>
+          </Col>
+
+          <Col offset={2} span={14}>
+            <Input placeholder="Phone number" />
+          </Col>
+        </Row>
+
+        <SizeBox height={4} />
+
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Button type="primary" size="large">
+            Submit
+          </Button>
+        </div>
       </ContentContainer>
     </Container>
   );
